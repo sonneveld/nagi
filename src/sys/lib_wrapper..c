3194e0a25d0aabf3a998eaac7a29514e2461486d
Nick Sonneveld <nick@sonneveld.name>
2001-11-01 06:10:28 +0000

    ideas for new func names and wrappers

--- src/sys/lib_wrapper..c
+++ src/sys/lib_wrapper..c
@@ -31,3 +31,50 @@ void lib_delay()
 u32 lib_getticks()
 {
 }
+
+// malloc
+// free
+
+
+// SDL_WM_SetCaption(window_title, 0);
+// open audio
+// unlock audio
+// close audio
+// pause audio
+// pump events
+// threads
+
+
+// sdl surface
+/*
+
+
+
+
+
+
+
+events
+mouse
+*/
+
+/*
+sarien's
+
+typedef struct __GFX_DRIVER
+{
+	UINT16	(*init_video_mode)(void);
+	UINT16	(*deinit_video_mode)(void);
+	void	(*put_block)(UINT16 x1, UINT16 y1, UINT16 x2, UINT16 y2);
+	void	(*put_pixel)(UINT16 x, UINT16 y, UINT16 c);
+	void	(*poll_timer)(void);
+	UINT8	(*keypress)(void);
+	UINT16	(*get_key)(void);
+} __GFX_DRIVER;
+
+*/
+
+// gfx driver
+// sound driver
+// sys driver - timer, thread, blah
+// events driver - key, mouse, joy
