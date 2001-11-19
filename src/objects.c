6fbcce1d483d08f980a92430c7edad7d5e8d8c00
Nick Sonneveld <nick@sonneveld.name>
2001-11-19 03:12:46 +0000

    now tries the following method to open an object.. it goes onto the next one if any of the string pointers go beyond the file size.
    1) decrypt default
    2) swap decrypt options
    3) swap packing options
    4) swap decrypt options

--- src/objects.c
+++ src/objects.c
@@ -42,31 +42,30 @@ int object_file_load()
 {
 	u8 *obj_data;
 	u8 *ptr;
-	u16 count;
-	u8 item_size;
+	u16 count, i;
+	u8 field_size;
 	u16 head_anim_obj_max;
 	u16 head_string_offset;
 	u16 string_size;
 	u8 *string_ptr;
-	
-	if (c_game_res == RES_V3_AMIGA) item_size = 4;
-		else item_size = 3;
 	
 	if (inv_obj_table != 0)
 		object_file_unload();
 	
 	obj_data = file_load("object", 0);
 	
-	if (res_size < 3)
+	// 7 is an object with one item that has a name with no chars (nul) (3+3+1)
+	if (res_size < 7)
 	{
-		printf("object_file_load(): object incorrect size\n");
+		printf("object_file_load(): and the baby bear said, \"this object is too small!\"\n");
 		return 1;
 	}
 		
-	count = 3;
+	count = 5;
 	
 	while (count)
 	{
+
 		switch(count)
 		{
 			case 1: // still didn't work.. fail
@@ -75,15 +74,25 @@ int object_file_load()
 				return 2;
 				break;
 			
-			case 2: // 2nd go.. we'll have to decrypt now
-				printf("object_file_load(): default decryption option invalid.  trying opposite\n");
+			case 2: // decrypt again with different field size
+				decrypt_string(obj_data, obj_data + res_size);
+				break;
+			
+			case 3: // try swapping field sizes
+				field_size = c_game_object_packed?3:4;
+				break;
+			
+			case 4: // 2nd go.. we'll have to decrypt now
+				printf("object_file_load(): default options invalid.\nAttempting alternative decryption/packing methods...\n");
 				decrypt_string(obj_data, obj_data + res_size );
 				break;
 			
-			case 3: // first go round... decrypt if necessary
+			case 5: // first go round... decrypt if necessary
 				// decrypt or not decrypt
-				if (c_game_object_decrypt != 0)
+				if (c_game_object_packed != 0)
 					decrypt_string(obj_data, obj_data + res_size);
+				
+				field_size = c_game_object_packed?4:3;
 				break;
 				
 			default:
@@ -94,36 +103,35 @@ int object_file_load()
 		
 		// read name's offset
 		head_string_offset = load_le_16(obj_data);
-		if ((head_string_offset + item_size) >= res_size)
+		if ((head_string_offset + field_size) >= res_size)
 			goto obj_continue;	// decrypt and try again
 		
 		// read animated obj_max
-		if (c_game_res == RES_V3_AMIGA)
+		if (c_game_object_packed)
 			head_anim_obj_max = load_le_16(obj_data + 2) + 1;
 		else
 			head_anim_obj_max = obj_data[2] + 1;
 		
-		printf("object_file_load(): number of animated object's defined = %d\n", head_anim_obj_max);
-
 		// load up table
-		count = head_string_offset / item_size;
-		inv_obj_table_size = count;
-		if (count)
+		inv_obj_table_size = head_string_offset / field_size;
+		
+		if (inv_obj_table_size)
 		{
-			ptr = obj_data + item_size*(count);
-			inv_obj_table = (INV_OBJ *)a_malloc(count * sizeof(INV_OBJ));
-			while (count)
+			ptr = obj_data + field_size*(inv_obj_table_size);
+			inv_obj_table = (INV_OBJ *)a_malloc(inv_obj_table_size * sizeof(INV_OBJ));
+			i = inv_obj_table_size;
+			while (i)
 			{
-				count--;
-				inv_obj_table[count].name = load_le_16(ptr );
-				if ((inv_obj_table[count].name + item_size) >= res_size)
+				i--;
+				inv_obj_table[i].name = load_le_16(ptr);
+				if ((inv_obj_table[i].name + field_size) >= res_size)
 					goto obj_continue;	// decrypt and try again
-				inv_obj_table[count].name -= head_string_offset;
-				if (c_game_res == RES_V3_AMIGA)
-					inv_obj_table[count].location = load_le_16(ptr  + 2);
+				inv_obj_table[i].name -= head_string_offset;
+				if (c_game_object_packed)
+					inv_obj_table[i].location = load_le_16(ptr  + 2);
 				else
-					inv_obj_table[count].location = ptr[2];
-				ptr -= item_size;
+					inv_obj_table[i].location = ptr[2];
+				ptr -= field_size;
 			}
 		}
 		
@@ -132,8 +140,8 @@ int object_file_load()
 	}
 	
 	// bung in a '\0' at the end if one doesn't exist
-	string_size = res_size - head_string_offset - item_size ;
-	string_ptr = obj_data+item_size+head_string_offset;
+	string_size = res_size - head_string_offset - field_size ;
+	string_ptr = obj_data+field_size+head_string_offset;
 	
 	// create string stuff
 	if (string_ptr[string_size-1] != '\0')
@@ -156,6 +164,8 @@ int object_file_load()
 	
 	// call obj_table create
 	objtable_new(head_anim_obj_max);	// new view table baby
+	
+	printf("object_file_load(): number of animated object's defined = %d\n", head_anim_obj_max);
 	
 	return 0;
 }
