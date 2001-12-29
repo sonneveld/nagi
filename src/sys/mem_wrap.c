aca7f4a586e553e624950a9660e345c324aadb3d
Nick Sonneveld <nick@sonneveld.name>
2001-12-29 21:22:57 +0000

    very tidy

--- src/sys/mem_wrap.c
+++ src/sys/mem_wrap.c
@@ -3,7 +3,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int total = 0;
 
 void *a_malloc (size_t size)
 {
@@ -15,17 +14,11 @@ void *a_malloc (size_t size)
 		printf("malloc error\n");
 		exit(-1);
 	}
-
-	//	total += size;
-	//printf("m=%d ", size);
-	//printf("m=%X \n", (u32)m);
 	return m;
 }
 
 void a_free(void *m)
 {
-	//		total += size;
-	//printf("m=%d ", size);
-	//printf("f=%X \n", (u32)m);
 	free(m);
 }
+
