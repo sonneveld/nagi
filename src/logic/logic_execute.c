3774632977b779f9be9c2351f47e673ad5b44c60
Nick Sonneveld <nick@sonneveld.name>
2002-02-16 08:55:23 +0000

    feh

--- src/logic/logic_execute.c
+++ src/logic/logic_execute.c
@@ -11,7 +11,7 @@ _LogicExecute                    cseg     0000293C 000000BA
 // data += *(data++) + *(data++) IS WORSE YOU IDIOT!!!
 // ahem
 
-#define LOG_DEBUG 1
+#define LOG_DEBUG 0
 
 
 #include <stdio.h>
