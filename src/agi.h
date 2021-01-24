4a8cc37f422f4007e1da37b23aab11349c84c760
Nick Sonneveld <nick@sonneveld.name>
2021-01-24 18:35:33 +1100

    Use stdint types when defining u{8,16,32} and s{8,16,32}

--- src/agi.h
+++ src/agi.h
@@ -4,6 +4,8 @@
 // Important include files that just about every file uses
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
 #include "SDL.h"
 
 //#define alloca __builtin_alloca
@@ -45,12 +47,12 @@ typedef struct list_struct STACK;
 
 
 
-typedef unsigned char	u8;
-typedef unsigned short	u16;
-typedef unsigned long	u32;
-typedef signed char		s8;
-typedef signed short		s16;
-typedef signed long		s32;
+typedef uint8_t		u8;
+typedef uint16_t	u16;
+typedef uint32_t	u32;
+typedef int8_t		s8;
+typedef int16_t		s16;
+typedef int32_t		s32;
 
 #define V00_ROOM0		0
 #define V01_OLDROOM		1
