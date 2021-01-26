dd0665b618ef5eb9cea3498eb6efe0c5df3d7deb
Nick Sonneveld <nick@sonneveld.name>
2021-01-26 17:23:10 +1100

    endian: refactor
    now being more explicit, good compilers can optimise this away.

--- src/sys/endian.c
+++ src/sys/endian.c
@@ -15,55 +15,29 @@ big endian is motorola
 #include "endian.h"
 
 
-#if SDL_BYTEORDER == SDL_LIL_ENDIAN
-	// ----------- little endian -------------------------------------------------------
-	u16 load_le_16(void *data)
-	{
-		return *(  (u16 *)data  );
-	}
-	
-	u16 load_be_16(void *data)
-	{
-		return ((u8 *)data)[1] |  (     ((u16)((u8 *)data)[0]) << 8  );
-	}
-	
-	
-	void store_le_16(void *data, u16 value)
-	{
-		*(  (u16 *)data  ) = value;
-	}
-	
-	void store_be_16(void *data, u16 value)
-	{
-		((u8 *)data)[1] = value & 0xFF;
-		((u8 *)data)[0] = (value & 0xFF00) >> 8;
-	}
-	
-#else
-	
-	// ----------- big endian -------------------------------------------------------
-	// TODO: big endian is untested..  I hope it works
-	u16 load_le_16(void *data)
-	{
-		return ((u8 *)data)[0] |  (   ((u16)((u8 *)data)[1]) << 8  );
-	}
-	
-	u16 load_be_16(void *data)
-	{
-		return *(  (u16 *)data  );
-	}
-	
-	
-	void store_le_16(void *data, u16 value)
-	{
-		((u8 *)data)[0] = value & 0xFF;
-		((u8 *)data)[1] = (value & 0xFF00) >> 8;
-	}
-	
-	void store_be_16(void *data, u16 value)
-	{
-		*(  (u16 *)data  ) = value;
-	
-	}
-#endif
+u16 load_le_16(const void *data)
+{
+	const u8 *datab = (const u8 *)(data);
+	return (datab[1] << 8) | datab[0];
+}
 
+u16 load_be_16(const void *data)
+{
+	const u8 *datab = (const u8 *)(data);
+	return (datab[0] << 8) | datab[1];
+}
+
+
+void store_le_16(void *data, u16 value)
+{
+	u8 *datab = (u8 *)(data);
+	datab[0] = value & 0xFF;
+	datab[1] = (value >> 8) & 0xFF;
+}
+
+void store_be_16(void *data, u16 value)
+{
+	u8 *datab = (u8 *)(data);
+	datab[0] = (value >> 8) & 0xFF;
+	datab[1] = value & 0xFF;
+}
--- src/sys/endian.h
+++ src/sys/endian.h
@@ -1,8 +1,8 @@
 #ifndef NAGI_SYS_ENDIAN_H
 #define NAGI_SYS_ENDIAN_H
 
-extern u16 load_le_16(void *data);
-extern u16 load_be_16(void *data);
+extern u16 load_le_16(const void *data);
+extern u16 load_be_16(const void *data);
 extern void store_le_16(void *data, u16 value);
 extern void store_be_16(void *data, u16 value);
 
